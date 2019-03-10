8a9b4e1693312b8e99c5b82b251f9263f80397a5
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-10 20:21:53 +0100

    add addHeaders callback

--- src/useRemoteJson.ts
+++ src/useRemoteJson.ts
@@ -1,19 +1,39 @@
+import produce from "immer";
 import { useEffect, useState } from "react";
-
-export const useRemoteJson = (uri: string, placeholder?: any) => {
+export const useRemoteJson = (
+  uri: string,
+  placeholder?: any,
+  addHeaders?: () => Promise<HeadersInit>
+) => {
   const [data, setData] = useState<any>(placeholder ? placeholder : null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
+  const getHeaders = async () => {
+    const additionalHeaders = addHeaders ? await addHeaders() : {};
+    return produce(additionalHeaders, draft => {
+      draft["content-type"] = "application/json";
+    });
+  };
+
   useEffect(() => {
-    fetch(uri)
+    getHeaders()
+      .then(headers => fetch(uri, { headers }))
+      .then(r => {
+        if (r.ok) {
+          return r;
+        } else {
+          console.error("http error", r);
+          throw Error(r.statusText);
+        }
+      })
       .then(response => response.json())
       .then(data => {
         setData(data);
         setLoading(false);
       })
       .catch(e => {
-        setError(e);
+        setError(e.toString());
         setLoading(false);
       });
   }, [uri]);
